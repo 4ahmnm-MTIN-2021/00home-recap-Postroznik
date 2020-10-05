edbd837032fe89d5c188268a8e3e79c340cfa04d
Postroznik <postroznik@gmail.com>
2020-10-05 22:07:47 +0200

    Start working on add Numbers Script

---
+++ Assets/MyGame/Scripts/NumberScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberScript : MonoBehaviour
+{
+  
+    public InputField numberA;
+    public InputField numberB;
+    public Text result;
+
+    public void AddNumbers ()
+    {
+        int a = numberA.text.to
+    }
+ }
